465ba3b470b2558e7adfd7a62b1c24c466267ae1
HLDev <hunglaidangvn@gmail.com>
2019-12-24 13:49:45 +0700

    Crop image before handling

--- bca.py
+++ bca.py
@@ -18,17 +18,17 @@ MONGO_DB_LINK = "mongodb://localhost:27017/"
 MONGO_DB_NAME = "vlparceltracking"
 MONGO_DB_COLL = "vlChuteMngt"
 
-input_video = "input_images_and_videos/M02-20191207-20h59"
+input_video = "input_images_and_videos/20191130_M02_08h39"
 
-is_color_recognition_enabled = 0 
+is_color_recognition_enabled = 0
 
-detection_graph, category_index = backbone.set_model('output-22754', 'parcel.pbtxt')
+detection_graph, category_index = backbone.set_model('output-141516', 'parcel.pbtxt')
 
-roi_y = 640 # roi line position
+roi_y = 580 # roi line position
 
 # Count lines for couting chutes : [x_start, x_end, y_line]
-roi_chutes = [[562, 550, 719], [586, 416, 552], [604, 314, 418], [618, 238, 315], [630, 179, 240], [640, 130, 180],
-              [987, 544, 712], [953, 413, 540], [925, 314, 412], [900, 237, 311], [880, 180, 234], [862, 131, 176]]
+roi_chutes = [[132, 450, 619], [156, 316, 452], [174, 214, 318], [188, 138, 215], [200, 79, 140], [210, 030, 80],
+              [557, 444, 612], [523, 313, 440], [495, 214, 312], [470, 137, 211], [450, 80, 134], [432, 31, 76]]
 # the constant that represents the object counting area
 deviation = 25
 
--- utils/object_counting_module/object_counter_x_axis.py
+++ utils/object_counting_module/object_counter_x_axis.py
@@ -2,12 +2,12 @@ from utils.image_utils import image_saver
 
 is_vehicle_detected = [0]
 bottom_position_of_detected_vehicle = [0]
-roi_chutes = [[0, 562, 550, 719], [1, 586, 416, 552], [2, 604, 314, 418],
-              [3, 618, 238, 315], [4, 630, 179, 240], [5, 640, 130, 180],
-              [6, 987, 544, 712], [7, 953, 413, 540], [8, 925, 314, 412],
-              [9, 900, 237, 311], [10,880, 180, 234], [11,862, 131, 176]]
+roi_chutes = [[0, 132, 450, 619], [1, 156, 316, 452], [2, 174, 214, 318],
+              [3, 188, 138, 215], [4, 200, 79, 140],  [5, 210, 030, 80],
+              [6, 557, 444, 612], [7, 523, 313, 440], [8, 495, 214, 312],
+              [9, 470, 137, 211], [10,450, 80, 134],  [11,432, 31,  76]]
 ''' [chute no, x_line, ymin, ymax]'''
-def count_objects_x_axis(top, bottom, right, left, crop_img, roi_position, y_min, y_max, deviation):   
+def count_objects_x_axis(top, bottom, right, left, crop_img, roi_position, y_min, y_max, deviation):
         direction = "n.a." # means not available, it is just initialization
         isInROI = True # is the object that is inside Region Of Interest
         update_csv = False
